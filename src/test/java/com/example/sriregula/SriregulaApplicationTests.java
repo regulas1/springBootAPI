0ac1307ede50dd749a6d9130e08fdab181ebdd1d
regulas1 <sriregula@outlook.com>
2022-01-15 20:50:18 +1300

    Initialising springboot project

---
+++ src/test/java/com/example/sriregula/SriregulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sriregula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriregulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
